e32fd2e6c0d6fac36d36e3ded7a58309cc23ecf4
Ashishkumar Gupta <akumar9@walmartlabs.com>
2017-07-06 18:36:36 +0530

    initial commit of java spring app with maven

---
+++ src/test/java/org/springframework/samples/petclinic/it/NewPetFirstVisitIT.java
@@ -0,0 +1,298 @@
+package org.springframework.samples.petclinic.it;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Assume;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+
+public class NewPetFirstVisitIT {
+
+    private static final Logger logger = LoggerFactory.getLogger(NewPetFirstVisitIT.class);
+
+    private static boolean RUN_HTMLUNIT;
+
+    private static boolean RUN_IE;
+
+    private static boolean RUN_FIREFOX;
+
+    private static boolean RUN_CHROME;
+
+    private static boolean RUN_OPERA;
+
+    private static String SELENIUM_HUB_URL;
+
+    private static String TARGET_SERVER_URL;
+
+    @BeforeClass
+    public static void initEnvironment() {
+
+        RUN_HTMLUNIT = getConfigurationProperty(
+            "RUN_HTMLUNIT",
+            "test.run.htmlunit",
+            true);
+
+        logger.info("running the tests in HtmlUnit: " + RUN_HTMLUNIT);
+
+        RUN_IE = getConfigurationProperty(
+            "RUN_IE",
+            "test.run.ie",
+            false);
+
+        logger.info("running the tests in Internet Explorer: " + RUN_IE);
+
+        RUN_FIREFOX = getConfigurationProperty(
+            "RUN_FIREFOX",
+            "test.run.firefox",
+            false);
+
+        logger.info("running the tests in Firefox: " + RUN_FIREFOX);
+
+        RUN_CHROME = getConfigurationProperty(
+            "RUN_CHROME",
+            "test.run.chrome",
+            false);
+
+        logger.info("running the tests in Chrome: " + RUN_CHROME);
+
+        RUN_OPERA = getConfigurationProperty(
+            "RUN_OPERA",
+            "test.run.opera",
+            false);
+
+        logger.info("running the tests in Opera: " + RUN_OPERA);
+
+        SELENIUM_HUB_URL = getConfigurationProperty(
+            "SELENIUM_HUB_URL",
+            "test.selenium.hub.url",
+            "http://localhost:4444/wd/hub");
+
+        logger.info("using Selenium hub at: " + SELENIUM_HUB_URL);
+
+        TARGET_SERVER_URL = getConfigurationProperty(
+            "TARGET_SERVER_URL",
+            "test.target.server.url",
+            "http://localhost:58080/petclinic");
+
+        logger.info("using target server at: " + TARGET_SERVER_URL);
+    }
+
+    private static String getConfigurationProperty(String envKey, String sysKey, String defValue) {
+
+        String retValue = defValue;
+        String envValue = System.getenv(envKey);
+        String sysValue = System.getProperty(sysKey);
+        // system property prevails over environment variable
+        if (sysValue != null) {
+            retValue = sysValue;
+        } else if (envValue != null) {
+            retValue = envValue;
+        }
+        return retValue;
+    }
+
+    private static boolean getConfigurationProperty(String envKey, String sysKey, boolean defValue) {
+
+        boolean retValue = defValue;
+        String envValue = System.getenv(envKey);
+        String sysValue = System.getProperty(sysKey);
+        // system property prevails over environment variable
+        if (sysValue != null) {
+            retValue = Boolean.parseBoolean(sysValue);
+        } else if (envValue != null) {
+            retValue = Boolean.parseBoolean(envValue);
+        }
+        return retValue;
+    }
+
+    @Test
+    public void testHtmlUnit()
+        throws MalformedURLException, IOException {
+
+        Assume.assumeTrue(RUN_HTMLUNIT);
+
+        logger.info("executing test in htmlunit");
+
+        WebDriver driver = new HtmlUnitDriver();
+
+        try {
+            testNewPetFirstVisit(driver, TARGET_SERVER_URL);
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+    @Test
+    public void testIE()
+        throws MalformedURLException, IOException {
+
+        Assume.assumeTrue(RUN_IE);
+
+        logger.info("executing test in internet explorer");
+
+        Capabilities browser = DesiredCapabilities.internetExplorer();
+        WebDriver driver = new RemoteWebDriver(new URL(SELENIUM_HUB_URL), browser);
+
+        try {
+            testNewPetFirstVisit(driver, TARGET_SERVER_URL);
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+    @Test
+    public void testFirefox()
+        throws MalformedURLException, IOException {
+
+        Assume.assumeTrue(RUN_FIREFOX);
+
+        logger.info("executing test in firefox");
+
+        Capabilities browser = DesiredCapabilities.firefox();
+        WebDriver driver = new RemoteWebDriver(new URL(SELENIUM_HUB_URL), browser);
+
+        try {
+            testNewPetFirstVisit(driver, TARGET_SERVER_URL);
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+    @Test
+    public void testChrome()
+        throws MalformedURLException, IOException {
+
+        Assume.assumeTrue(RUN_CHROME);
+
+        logger.info("executing test in chrome");
+
+        Capabilities browser = DesiredCapabilities.chrome();
+        WebDriver driver = new RemoteWebDriver(new URL(SELENIUM_HUB_URL), browser);
+
+        try {
+            testNewPetFirstVisit(driver, TARGET_SERVER_URL);
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+    @Test
+    public void testOpera()
+        throws MalformedURLException, IOException {
+
+        Assume.assumeTrue(RUN_OPERA);
+
+        logger.info("executing test in opera");
+
+        Capabilities browser = DesiredCapabilities.operaBlink();
+        WebDriver driver = new RemoteWebDriver(new URL(SELENIUM_HUB_URL), browser);
+
+        try {
+            testNewPetFirstVisit(driver, TARGET_SERVER_URL);
+        } finally {
+            if (driver != null) {
+                driver.quit();
+            }
+        }
+    }
+
+    public void testNewPetFirstVisit(final WebDriver driver, final String baseUrl) {
+
+        driver.get(baseUrl);
+
+        // wait for the application to get fully loaded
+        WebElement findOwnerLink = (new WebDriverWait(driver, 5)).until(
+            (Function<WebDriver, WebElement>) d -> d.findElement(By.linkText("Find owner")));
+
+        findOwnerLink.click();
+
+        (new WebDriverWait(driver, 5)).until((Predicate<WebDriver>) d
+            -> d.getCurrentUrl().startsWith(baseUrl + "/owners/search"));
+
+        driver.findElement(By.id("lastName")).clear();
+        driver.findElement(By.id("lastName")).sendKeys("Schroeder");
+        driver.findElement(By.id("findowners")).click();
+
+        (new WebDriverWait(driver, 5)).until(
+            (Predicate<WebDriver>) d -> d.getCurrentUrl().equals(baseUrl + "/owners/9"));
+
+        assertTrue(driver.findElement(By.id("main")).getText().contains("David Schroeder"));
+        driver.findElement(By.linkText("Add New Pet")).click();
+
+        (new WebDriverWait(driver, 5)).until(
+            (Predicate<WebDriver>) d -> d.getCurrentUrl().equals(baseUrl + "/owners/9/pets/new"));
+
+        driver.findElement(By.id("name")).clear();
+        driver.findElement(By.id("name")).sendKeys("Mimi");
+        driver.findElement(By.id("birthDate")).clear();
+        driver.findElement(By.id("birthDate")).sendKeys("2011-10-02");
+        driver.findElement(By.id("addpet")).click();
+
+        (new WebDriverWait(driver, 5)).until(
+            (Predicate<WebDriver>) d -> d.getCurrentUrl().startsWith(baseUrl + "/owners/9")
+                && !d.getCurrentUrl().contains("pets/new"));
+
+        assertTrue(driver.findElement(By.id("main")).getText().contains("David Schroeder"));
+        assertTrue(driver.findElement(By.id("main")).getText().contains("Mimi"));
+        assertTrue(driver.findElement(By.id("main")).getText().contains("2011-10-02"));
+        driver.findElement(By.linkText("Add Visit")).click();
+
+        (new WebDriverWait(driver, 5)).until(
+            (Predicate<WebDriver>) d -> d.getCurrentUrl().startsWith(baseUrl + "/owners/9/pets")
+                && d.getCurrentUrl().contains("visits/new"));
+
+        driver.findElement(By.id("date")).clear();
+        driver.findElement(By.id("date")).sendKeys("2012-03-15");
+        driver.findElement(By.id("description")).clear();
+        driver.findElement(By.id("description")).sendKeys("rabies shot");
+        driver.findElement(By.id("addvisit")).click();
+
+        (new WebDriverWait(driver, 5)).until(
+            (Predicate<WebDriver>) d -> d.getCurrentUrl().startsWith(baseUrl + "/owners/9")
+                && !d.getCurrentUrl().contains("visits/new"));
+
+        assertTrue(driver.findElement(By.id("main")).getText().contains("David Schroeder"));
+        assertTrue(driver.findElement(By.id("main")).getText().contains("Mimi"));
+        assertTrue(driver.findElement(By.id("main")).getText().contains("2011-10-02"));
+        assertTrue(driver.findElement(By.id("main")).getText().contains("rabies shot"));
+        driver.findElement(By.linkText("Edit Pet")).click();
+
+        (new WebDriverWait(driver, 5)).until(
+            (Predicate<WebDriver>) d -> d.getCurrentUrl().startsWith(baseUrl + "/owners/9/pets")
+                && d.getCurrentUrl().contains("edit"));
+
+        driver.findElement(By.id("deletepet")).click();
+
+        (new WebDriverWait(driver, 5)).until(
+            (Predicate<WebDriver>) d -> d.getCurrentUrl().equals(baseUrl + "/owners/9"));
+
+        assertTrue(driver.findElement(By.id("main")).getText().contains("David Schroeder"));
+        assertFalse(driver.findElement(By.id("main")).getText().contains("Mimi"));
+    }
+}
